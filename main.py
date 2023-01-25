b23161eafd75d2c3683a82bcc21c016b1da9e965
aindriu18 <aindriuinglis@gmail.com>
2023-01-25 13:41:12 +0000

    Made changes so that when the programme is run, the ouput displays the article name, article link and how many votes the article has.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ In this case, all we want to grab is any link from Hacker.com that has 100 point
 
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 # web browser without the actual window
 res = requests.get("https://news.ycombinator.com/")
@@ -29,31 +30,44 @@ soup = BeautifulSoup(res.text, 'html.parser')
 #print(soup.select('.score'))
 
 article_title = soup.select('.titleline > a')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
 # will receive links and votes
-def create_custom_hacker_news(links, votes):
+def create_custom_hacker_news(links, subtext):
 
     # new hacker news list which is empty - we only want text and no HTML.
     news = []
 
     # enumerate gives us an index
+    # it also allows us to grab the subtext from the links list
+    # where we use links within the for loop, we can actually replace with item
     for index, item in enumerate(links):
         # grab the title of each link - getText() gets the text inside of the tag
         title = links[index].getText()
         # grabbing the href. If there are none, or link broken, default to None.
         href = links[index].get('href', None)
+
         # as this stands, there is a chance that we may encounter a list index out of bounds error.
         # this is due to the fact that some articles may not have any points (rare, but possible).
         # therefore, our programme can think there are more article links than article points.
-        # we convert votes to an int and replace the text with an empty string
-        points = int(votes[index].getText().replace(' points', ""))
-        # use a dictionary to append title and href link to new list
-        news.append({'Article Title': title, "Article Link": href})
+        # the vote variable and the if statement below fixes this.
+        # instead of grabbing points, we are going to get the class subtext from Hacker News page.
+        # Within this subtext class on the Hacker News page is the score. We are able to check if there is a score
+        vote = subtext[index].select('.score')
+        if len(vote):
+
+            # we convert votes to an int and replace the text with an empty string
+            points = int(vote[0].getText().replace(' points', ""))
+
+            # only display articles with 100 points or more
+            if points > 99:
+                # use a dictionary to append title and href link to new list
+                news.append({'Article Title': title, "Article Link": href, 'Votes': points})
 
     return news
 
-print(create_custom_hacker_news(article_title, votes))
+# pprint is a module that makes the output easier to reade
+pprint.pprint(create_custom_hacker_news(article_title, subtext))
 
 
 
